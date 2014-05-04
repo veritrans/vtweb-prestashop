014d9fe0a1350dbef77001d0d944fba1c18d5fce
Ismail Faruqi <ismailfaruqi@gmail.com>
2014-05-04 10:32:45 +0700

    First working notification for 1.4

--- notification.php
+++ notification.php
@@ -7,8 +7,27 @@ $root_dir = str_replace('modules/veritranspay', '', dirname($_SERVER['SCRIPT_FIL
 include_once($root_dir.'/config/config.inc.php');
 require_once 'library/veritrans_notification.php';
 
+function getTransaction($request_id)
+{
+  $sql = 'SELECT *
+      FROM `'._DB_PREFIX_.'vt_transaction`
+      WHERE `request_id` = \''.$request_id.'\'';
+  $result = Db::getInstance()->getRow($sql);
+  return $result; 
+}
+
+function validate($id_transaction, $id_order, $order_status)
+{
+  $sql = 'INSERT INTO `'._DB_PREFIX_.'vt_validation`
+      (`id_order`, `id_transaction`, `order_status`)
+      VALUES ('.(int)$id_transaction.',
+          '.(int)$id_order.',
+          \''.$order_status.'\')';
+  Db::getInstance()->Execute($sql);
+}
+
 $veritrans_notification = new VeritransNotification();
-$transaction = $this->getTransaction($veritrans_notification->orderId);
+$transaction = getTransaction($veritrans_notification->orderId);
 $order_id = $veritrans_notification->orderId;
 
 $token_merchant = $transaction['token_merchant'];
